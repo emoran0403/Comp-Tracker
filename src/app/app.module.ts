d1db738db4bb7cd5c25d2786b665ec60d90f37a8
Eric Moran <emoran0403@gmail.com>
2022-11-21 16:43:42 -0500

    comments styled with angular material cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+// Angular Material
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatDividerModule } from '@angular/material/divider';
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LookupComponent } from './components/lookup/lookup.component';
-
-import { SkillService } from './services/skill.service';
-import { QuestService } from './services/quest.service';
-import { StorageService } from './services/storage.service';
-import { JsonPlaceHolderService } from './services/json-place-holder.service';
 import { ButtonComponent } from './components/button/button.component';
 import { UserComponent } from './components/json/user/user.component';
 import { TodoComponent } from './components/json/todo/todo.component';
@@ -23,7 +21,11 @@ import { PhotoComponent } from './components/json/photo/photo.component';
 import { AlbumComponent } from './components/json/album/album.component';
 import { CommentComponent } from './components/json/comment/comment.component';
 import { PostComponent } from './components/json/post/post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Services
+import { SkillService } from './services/skill.service';
+import { QuestService } from './services/quest.service';
+import { StorageService } from './services/storage.service';
+import { JsonPlaceHolderService } from './services/json-place-holder.service';
 
 @NgModule({
   declarations: [
@@ -47,6 +49,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatCardModule,
+    MatDividerModule,
   ],
   providers: [
     SkillService,
