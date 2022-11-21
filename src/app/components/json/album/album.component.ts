bd12fc5b4470dd3f464743c81da41d056445acfd
Eric Moran <emoran0403@gmail.com>
2022-11-21 14:53:29 -0500

    albums displaying

--- src/app/components/json/album/album.component.ts
+++ src/app/components/json/album/album.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Album } from '../../header/header.component';
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css']
+  styleUrls: ['./album.component.css'],
 })
 export class AlbumComponent implements OnInit {
+  @Input() album!: Album;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
