ec5785c2f57ad9eb58d0dc5853385525a4a762e0
Eric Moran <emoran0403@gmail.com>
2022-11-26 11:09:56 -0500

    added express package, added skills and quests routes

--- src/server/routes/api/questRoutes.ts
+++ src/server/routes/api/questRoutes.ts
@@ -7,16 +7,16 @@ const questRouter = express.Router();
 // Get quests from RS API
 
 questRouter.get('/', async (req, res) => {
+  const username = req.body.username;
+  const apiURL: string = `https://apps.runescape.com/runemetrics/quests?user=${username}`;
+
   try {
-    // const data = await db.Blogs.readAllBlogs(); // Read all Blogs
-    // res.status(200).json(data); // send 200 and the data
+    const data = await fetch(apiURL).then((res) => res.json());
+    res.status(200).json(data); // send 200 and the data
   } catch (error) {
-    // if (error.sqlMessage) {
-    //   console.log(`\n${error.sqlMessage}\n`); // log the sql error if there is one
-    // }
-    // console.log(`Get All Blogs error...\n`);
-    // console.error(error); // if an error happens, log the error
-    // res.status(500).json({ message: 'Get All Blogs failed, big R.I.P' }); // send status of 500
+    console.log(`Get quests error...\n`);
+    console.error(error); // if an error happens, log the error
+    res.status(500).json({ message: 'Get quests failed, big R.I.P' }); // send status of 500
   }
 });
 
--- src/server/routes/api/skillRoutes.ts
+++ src/server/routes/api/skillRoutes.ts
@@ -7,16 +7,16 @@ const skillRouter = express.Router();
 // Get skills from RS API
 
 skillRouter.get('/', async (req, res) => {
+  const username = req.body.username;
+  const apiURL: string = `https://secure.runescape.com/m=hiscore/index_lite.ws?player=${username}`;
+
   try {
-    // const data = await db.Blogs.readAllBlogs(); // Read all Blogs
-    // res.status(200).json(data); // send 200 and the data
+    const data = await fetch(apiURL).then((res) => res.json());
+    res.status(200).json(data); // send 200 and the data
   } catch (error) {
-    // if (error.sqlMessage) {
-    //   console.log(`\n${error.sqlMessage}\n`); // log the sql error if there is one
-    // }
-    // console.log(`Get All Blogs error...\n`);
-    // console.error(error); // if an error happens, log the error
-    // res.status(500).json({ message: 'Get All Blogs failed, big R.I.P' }); // send status of 500
+    console.log(`Get skills error...\n`);
+    console.error(error); // if an error happens, log the error
+    res.status(500).json({ message: 'Get skills failed, big R.I.P' }); // send status of 500
   }
 });
 
